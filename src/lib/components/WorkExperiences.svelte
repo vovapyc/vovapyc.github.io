85f56502b8c153653cdc1000cc591129a883fdc1
Vova Pytsiuk <vovawed@gmail.com>
2025-08-11 16:55:51 -0700

    Add svelte files

---
+++ src/lib/components/WorkExperiences.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Card from './Card.svelte';
+</script>
+
+<Card title="👔 Work experience">
+	<ol class="relative border-s border-gray-200 dark:border-gray-700">
+		<slot />
+	</ol>
+</Card>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	let { children } = $props();
+	import "../app.css";
+</script>
+
+<svelte:head>
+	<link
+		rel="icon"
+		href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🧑‍💻</text></svg>"
+	/>
+	<title>Volodymyr Pytsiuk</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</svelte:head>
+
+{@render children?.()}
